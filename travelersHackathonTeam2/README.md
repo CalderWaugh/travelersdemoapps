a4a4655ffe9ec39241f591d6b0be7a9894fa5035
Saravanan Kuppusamy <saravanan_kuppusamy@yahoo.com>
2023-08-02 14:27:22 -0400

    added MERN examples and projects

---
+++ travelersHackathonTeam2/README.md
@@ -0,0 +1,2 @@
+# Web Age Hackathon - Feedback App
+
---
+++ travelersHackathonTeam4/enterprise-react-app/src/components/home.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from "react";
+import {useParams} from 'react-router-dom';
+
+
+function HomePage(){
+    return(
+        <div>
+            <h1>Welcome!</h1>
+        </div>
+    )
+
+}
+
+export default HomePage;
